5ae6c2d944cfdc4388bf66da663963d2989ab4ac
jayyrt <jayy.rt19@gmail.com>
2021-10-12 13:51:50 -0600

    complete

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bc = require("./controllers/books_controller.js");
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/books', bc.read);
+app.post('/api/books', bc.create);
+app.put('/api/books/:id', bc.update);
+app.delete('/api/books/:id', bc.delete);
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
